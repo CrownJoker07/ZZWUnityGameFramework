a0fb1f41c757966973b4c63e648b4f3ca32287e1
ZhuangZewei <3462294546@qq.com>
2024-11-30 18:27:38 +0800

    feat： mono单例支持挂载场景和不销毁挂载场景两种方式

--- Project/Assets/Scripts/GameFramework/Singleton/MonoSingletonT.cs
+++ Project/Assets/Scripts/GameFramework/Singleton/MonoSingletonT.cs
@@ -1,19 +1,40 @@
 using UnityEngine;
 
-public abstract class MonoSingleton<T>  : MonoBehaviour where T : MonoBehaviour
+public abstract class MonoSingleton<T>  : MonoBehaviour where T : MonoSingleton<T>
 {
+    protected virtual bool isDontDestroyOnLoad => true;
     private static T _instance;
 
     public static T instance
     {
         get
         {
+            if (_instance == null)
+            {
+                GameObject gameObject = new GameObject(typeof(T).Name);
+
+                DontDestroyOnLoad(gameObject);
+                
+                T temT = gameObject.AddComponent<T>();
+
+                if (!temT.isDontDestroyOnLoad)
+                {
+                    Debug.LogError($"typeName:{typeof(T).Name} is not isDontDestroyOnLoad, but it created DontDestroyOnLoad");
+                    return null;
+                }
+                
+                _instance = temT;
+            }
+            
             return _instance;
         }
     }
 
     protected virtual void Awake()
     {
-        _instance = this as T;
+        if (!isDontDestroyOnLoad)
+        {
+            _instance = this as T;
+        }
     }
 }
