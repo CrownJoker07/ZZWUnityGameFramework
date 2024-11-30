61ace0cb666c3f248881cccbf3546b2786cf2fe7
ZhuangZewei <3462294546@qq.com>
2024-11-30 23:52:37 +0800

    feat ： 优化Mono单例逻辑，简化逻辑

--- Project/Assets/Scripts/GameFramework/Singleton/MonoSingletonT.cs
+++ Project/Assets/Scripts/GameFramework/Singleton/MonoSingletonT.cs
@@ -1,40 +1,37 @@
 using UnityEngine;
 
-public abstract class MonoSingleton<T>  : MonoBehaviour where T : MonoSingleton<T>
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
 {
-    protected virtual bool isDontDestroyOnLoad => true;
     private static T _instance;
 
     public static T instance
     {
         get
         {
-            if (_instance == null)
-            {
-                GameObject gameObject = new GameObject(typeof(T).Name);
-
-                DontDestroyOnLoad(gameObject);
-                
-                T temT = gameObject.AddComponent<T>();
-
-                if (!temT.isDontDestroyOnLoad)
-                {
-                    Debug.LogError($"typeName:{typeof(T).Name} is not isDontDestroyOnLoad, but it created DontDestroyOnLoad");
-                    return null;
-                }
-                
-                _instance = temT;
-            }
+            if (_instance != null) return _instance;
             
+            GameObject gameObject = new GameObject(typeof(T).Name);
+
+            DontDestroyOnLoad(gameObject);
+
+            T temT = gameObject.AddComponent<T>();
+
+            _instance = temT;
+
             return _instance;
         }
     }
 
     protected virtual void Awake()
     {
-        if (!isDontDestroyOnLoad)
+        if (_instance == null)
         {
             _instance = this as T;
+        }
+        else
+        {
+            Debug.LogError(
+                $"typeName:{typeof(T).Name} is Already Exist");
         }
     }
 }
