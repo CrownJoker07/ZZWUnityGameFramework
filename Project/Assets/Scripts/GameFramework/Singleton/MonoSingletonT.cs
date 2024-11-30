cc0d8ffb742d528306b3130778a0056fc9b5a7c8
ZhuangZewei <3462294546@qq.com>
2024-11-30 17:47:28 +0800

    feat ： 添加通用单例、Mono通用单例

---
+++ Project/Assets/Scripts/GameFramework/Singleton/MonoSingletonT.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MonoSingleton<T>  : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        _instance = this as T;
+    }
+}
