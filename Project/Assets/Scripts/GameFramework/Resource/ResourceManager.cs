bf55a38521c4d969584085ad5817ebf5bab46b7a
ZhuangZewei <3462294546@qq.com>
2024-12-21 22:37:55 +0800

    feat：添加部分资源管理器

---
+++ Project/Assets/Scripts/GameFramework/Resource/ResourceManager.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+/*
+ * ResourceManager 版本: V1.0.0，设计思路：
+ */
+public class ResourceManager : MonoSingleton<ResourceManager>
+{
+    public enum ResourceLoadMode
+    {
+        None = 0,
+        AssetBundle = 1,
+        Editor = 2,
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void LoadAssetAsync<T>(string assetPath, Action<T> successAction, Action failureAction = null)
+        where T : UnityEngine.Object
+    {
+        T temT = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(assetPath);
+
+        if (temT != null)
+        {
+            successAction?.Invoke(temT);
+        }
+        else
+        {
+            failureAction?.Invoke();
+        }
+    }
+}
