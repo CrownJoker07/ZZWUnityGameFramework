83bcce1b32ab1af77fb26ea6674ab06321485905
ZhuangZewei <3462294546@qq.com>
2025-05-07 17:19:18 +0800

    feat : 迁移音频设置逻辑进框架

---
+++ Editor/AssetPostprocessor/AudioPostprocessor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AudioPostprocessor : AssetPostprocessor {
+    public void OnPreprocessAudio() {
+        AudioImporter importer = (AudioImporter) assetImporter;
+        importer.forceToMono = true;
+    }
+}
